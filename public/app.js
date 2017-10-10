3f92d514a65f9121f0a87a950b2da4da5961dab6
Kieran Chadwick <kieran.chadwick@outlook.com>
2017-10-10 23:02:06 +0100

    Client reads and converts blobs

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 $(document).ready(() => init());
 
+let reader;
 let logsTableBody;
 
 function init() {
@@ -8,19 +9,37 @@ function init() {
     ws.onopen = () => console.log("Connection opened");
     
     ws.onmessage = e => {
-        console.log(e.data);
-        addLog(e.data);
+        if(e.data instanceof Blob) {
+            reader.readAsText(e.data);
+            reader.addEventListener('loadend', e => {
+                console.log(e);
+                const text = e.srcElement.result;
+                addLog(text);
+            });
+        } else {
+            addLog(e.data);
+        }
     }
+
+    reader = new FileReader();
     
     logsTableBody = $('#logs').find('tbody');
 }
 
 function addLog(message) {
-    logsTableBody.prepend(`
-        <tr>
-            <td>0</td>
-            <td>DEBUG</td>
-            <td>${message}</td>
-        </tr>
-    `);
+    if(message instanceof Blob) {
+        reader.readAsText(message);
+        reader.addEventListener('loadend', e => {
+            const text = e.srcElement.result;
+            console.log(text);
+        });
+    } else {
+        logsTableBody.prepend(`
+            <tr>
+                <td>0</td>
+                <td>DEBUG</td>
+                <td>${message}</td>
+            </tr>
+        `);
+    }
 }
